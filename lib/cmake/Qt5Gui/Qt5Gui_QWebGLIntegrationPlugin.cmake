85c292e65a102c2d1226a9655ec983236231bf10
kristof@jimber.org <kristof@jimber.org>
2018-04-04 15:21:56 +0200

    temp files removed

--- lib/cmake/Qt5Gui/Qt5Gui_QWebGLIntegrationPlugin.cmake
+++
@@ -1,6 +0,0 @@
-
-add_library(Qt5::QWebGLIntegrationPlugin MODULE IMPORTED)
-
-_populate_Gui_plugin_properties(QWebGLIntegrationPlugin RELEASE "platforms/libqwebgl.so")
-
-list(APPEND Qt5Gui_PLUGINS Qt5::QWebGLIntegrationPlugin)
--- mkspecs/modules-inst/qt_plugin_qwebgl.pri
+++
@@ -1,5 +0,0 @@
-QT_PLUGIN.qwebgl.TYPE = platforms
-QT_PLUGIN.qwebgl.EXTENDS = -
-QT_PLUGIN.qwebgl.DEPENDS = s,-private$,_private,g
-QT_PLUGIN.qwebgl.CLASS_NAME = QWebGLIntegrationPlugin
-QT_PLUGINS += qwebgl
